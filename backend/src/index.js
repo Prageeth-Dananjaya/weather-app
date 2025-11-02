7328d9e5164cf7623db3a877ad15bc1346c4d106
prageeth dananjaya <e19431@eng.pdn.ac.lk>
2025-11-02 08:02:08 +0530

    feat(backend): add routes to fetch weather data

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const weatherRoute = require("./routes/weather");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/weather", weatherRoute);
 
 const PORT = process.env.PORT || 4000;
 
---
+++ backend/src/routes/weather.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const { getCities, getWeatherByCityCode } = require("../services/openWeather");
+
+router.get("/", async (_, res) => {
+  try {
+    const cities = getCities();
+    const weatherData = await Promise.all(
+      cities.map((code) => getWeatherByCityCode(code))
+    );
+    res.json(weatherData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Failed to fetch weather data" });
+  }
+});
+
+router.get("/:cityCode", async (req, res) => {
+  try {
+    const data = await getWeatherByCityCode(req.params.cityCode);
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+module.exports = router;
